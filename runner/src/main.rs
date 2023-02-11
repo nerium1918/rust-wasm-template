71f0c77a040ca84da7d35fdd02309f0d27a558fa
Petko <pavlovskipetko@gmail.com>
2023-02-11 16:54:49 +0200

    Initial commit.

---
+++ runner/src/main.rs
@@ -0,0 +1,18 @@
+use wasmer::{Store, Module, Instance, imports};
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let mut store = Store::default();
+    let module = Module::from_file(&store, "/Users/petko/misc/wasm-rust/compiler/pkg/library_bg.wasm")?;
+
+    let import_object = imports! {
+        "fd_write" => {}
+    };
+    let instance = Instance::new(&mut store, &module, &import_object)?;
+
+    let basic = instance.exports.get_function("basic")?;
+    let result = basic.call(&mut store, &[])?[0].to_string();
+    println!("Basic is {}", result);
+
+    Ok(())
+}
